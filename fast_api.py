ed4e79ab2a081a56939703f5060477c6f868744c
REVANTH BN <rbanalan@andrew.cmu.edu>
2023-03-06 04:24:50 -0800

    Adding v1 of files for FastAPI on Render

---
+++ fast_api.py
@@ -0,0 +1,36 @@
+import os 
+import openai
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+	allow_methods=["GET"],
+    allow_origins=["*"],
+)
+
+class User_input(BaseModel):
+    prompt: str
+    key: str
+
+@app.post("/query")
+async def query_OpenAI(input: User_input):
+    print("I'm here")
+    openai.api_key = input.key
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=input.prompt,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+        )
+    # Extracting specific keys from response
+    result_stats = dict((k, response[k]) for k in ['id', 'object','created','model','usage']
+            if k in response)
+    text_out = response.choices[0].text
+    return JSONResponse(content={'stats': result_stats, 'output': text_out, 'detail': "Successful"})
